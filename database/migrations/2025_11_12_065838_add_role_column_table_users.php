50b6eae45984ea9b8039273e2e32284237b85d43
umair-sajjad <umairwritiner@gmail.com>
2025-11-12 13:37:56 +0500

    added necassary pages for UI and added role column

--- database/migrations/2025_11_12_065838_add_role_column_table_users.php
+++ database/migrations/2025_11_12_065838_add_role_column_table_users.php
@@ -11,7 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('role')->default('student');
+        });
     }
 
     /**
